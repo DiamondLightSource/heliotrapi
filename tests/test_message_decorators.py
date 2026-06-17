b395fb40dfc3af137c20114c41d824d5909e3c69
RJCD-Diamond <richard.dixey@diamond.ac.uk>
2026-06-17 17:19:23 +0000

    add extra test

--- tests/test_message_decorators.py
+++ tests/test_message_decorators.py
@@ -124,3 +124,37 @@ def test_message_analysis_decorators_returns_none_without_used_decorators(
     job = listener.stomp_message_to_request(message)
 
     assert job is None
+
+
+@pytest.mark.parametrize(
+    "message, analysis_name",
+    [
+        (START_MESSAGE, START_MESSAGE_ANALYSIS_NAME),
+        (STOP_MESSAGE, STOP_MESSAGE_ANALYSIS_NAME),
+        (STARTED_NEXUS_MESSAGE, STARTED_NEXUS_ANALYSIS_NAME),
+        (FINISHED_NEXUS_MESSAGE, FINISHED_NEXUS_ANALYSIS_NAME),
+    ],
+)
+def test_message_analysis_decorators_fails_with_bad_args_but_loads(
+    message: dict, analysis_name: str
+):
+
+    @start_message_analysis
+    def start_analysis(mymessage: StartMessage):
+        pass
+
+    @stop_message_analysis
+    def stop_analysis(message):
+        pass
+
+    @started_nexus_analysis
+    def start_nexus_analysis(message: StartMessage):
+        pass
+
+    @updated_nexus_analysis
+    def update_nexus_analysis(my_message: NexusMessage):
+        pass
+
+    @finished_nexus_analysis
+    def stop_nexus_analysis(message: NexusMessage, other_args: str):
+        pass
