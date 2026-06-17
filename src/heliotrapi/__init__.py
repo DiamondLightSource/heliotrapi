214d054134b698bb24cf885c2e6da66427530d93
RJCD-Diamond <richard.dixey@diamond.ac.uk>
2026-06-17 15:51:27 +0000

    add decorators and messages to init

--- src/heliotrapi/__init__.py
+++ src/heliotrapi/__init__.py
@@ -8,8 +8,25 @@
 
 from heliotrapi.analysis_core.decorator import (
     analysis,
+    finished_nexus_analysis,
+    start_message_analysis,
+    started_nexus_analysis,
+    stop_message_analysis,
+    updated_nexus_analysis,
 )
+from heliotrapi.task_queue.message_models import NexusMessage, StartMessage, StopMessage
 
 from ._version import __version__
 
-__all__ = ["__version__", "analysis"]
+__all__ = [
+    "__version__",
+    "analysis",
+    "finished_nexus_analysis",
+    "start_message_analysis",
+    "started_nexus_analysis",
+    "stop_message_analysis",
+    "updated_nexus_analysis",
+    "NexusMessage",
+    "StartMessage",
+    "StopMessage",
+]
