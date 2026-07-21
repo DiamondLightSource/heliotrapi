774514de55a6c23157fe167990c75b2b3abd9eff
RJCD-Diamond <richard.dixey@diamond.ac.uk>
2026-07-21 09:34:36 +0000

    add streaming bits to own file

--- src/heliotrapi/api/routes.py
+++ src/heliotrapi/api/routes.py
@@ -1,7 +1,5 @@
-import asyncio
 import inspect
 import json
-from collections.abc import Callable
 from typing import Any
 from uuid import UUID
 
@@ -26,10 +24,9 @@ from heliotrapi.models import (
     AnalysisResponse,
     AnalysisResult,
     AnalysisStreamRequest,
-    StreamUpdate,
 )
 from heliotrapi.task_queue import QueueManager
-from heliotrapi.task_queue.manager import convert_inputs, get_function_annotations
+from heliotrapi.task_queue.streaming import create_stream
 
 ROUTER = APIRouter()
 
@@ -142,48 +139,8 @@ async def get_all_results(request: Request):
     return results
 
 
-async def run_stream_analysis(
-    analysis_fn: Callable,
-    inputs: dict[str, Any],
-    update_interval: float,
-    max_iterations: int,
-):
-
-    iter = 0
-
-    while iter < max_iterations:
-        result_value = await analysis_fn(**inputs)  # actually run job
-        assert isinstance(result_value, (float | int))
-
-        update = StreamUpdate(x=iter, y=result_value)
-
-        iter = iter + 1
-
-        yield update.model_dump()
-
-        await asyncio.sleep(update_interval)
-
-
-async def create_stream(job: AnalysisStreamRequest):
-    analysis_fn = get_analysis(job.analysis_name)
-    annotations = get_function_annotations(analysis_fn)
-    converted_inputs = convert_inputs(job.inputs, annotations)
-    update_interval = job.update_interval
-    max_iterations = job.max_iterations
-
-    logger.info(job.inputs)
-
-    async for point in run_stream_analysis(
-        analysis_fn=analysis_fn,
-        inputs=converted_inputs,
-        update_interval=update_interval,
-        max_iterations=max_iterations,
-    ):
-        yield point
-
-
 @ROUTER.get(STREAM_ROUTE)
-async def stream(request: Request, job: AnalysisStreamRequest):
+async def stream(request: Request, job: AnalysisStreamRequest) -> StreamingResponse:
     """
     Server-Sent Events endpoint
     """
